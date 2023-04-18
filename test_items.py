dcb0d8fa02ed6fd28e1e4ace5b0238c00c2a38d7
Igor desktop <igorkushnarenko23@gmail.com>
2023-04-18 17:04:45 +0300

    Added confest.py and test_items.py Added logic for launching browser on different languages

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = 'https://docs.pytest.org/en/latest/explanation/flaky.html#plugins'
+
+
+def test_visibility_of_button_for_adding_to_basket(browser):
+    browser.get(link)
+    button_for_adding_to_basket = browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(button_for_adding_to_basket) == 1, 'There is no button for adding to basket on page or selector doesn"t unique'
